440b4988b36eaf70a536934ef23966a6031e0376
abdiranman <abdirahman.abdi330@.com>
2023-08-15 13:47:57 -0400

    add

--- input.js
+++ input.js
@@ -17,20 +17,30 @@ const setupInput = function (conn) {
 };
 
 const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  
-  // Sending movement commands to the server
-  if (key === 'w') {
-    connection.write("Move: up");
-  } else if (key === 'a') {
-    connection.write("Move: left");
-  } else if (key === 's') {
-    connection.write("Move: down");
-  } else if (key === 'd') {
-    connection.write("Move: right");
-  }
-};
+    if (key === '\u0003') {
+      process.exit();
+    }
+    
+    // Sending movement commands to the server
+    if (key === 'w') {
+      connection.write("Move: up");
+    } else if (key === 'a') {
+      connection.write("Move: left");
+    } else if (key === 's') {
+      connection.write("Move: down");
+    } else if (key === 'd') {
+      connection.write("Move: right");
+    }
+    
+    // Sending chat messages to the server
+    if (key === '1') {
+      connection.write("Say: Hello, everyone!");
+    } else if (key === '2') {
+      connection.write("Say: Having a blast!");
+    } else if (key === '3') {
+      connection.write("Say: Snake time!");
+    }
+  };
+
 
 module.exports = { setupInput };
