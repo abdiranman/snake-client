7b0854dadf55b823eb536afc4bda65d1faf420ff
abdiranman <abdirahman.abdi330@.com>
2023-08-15 13:31:11 -0400

    add

--- input.js
+++ input.js
@@ -1,23 +1,31 @@
-// Setup user input handling
-const setupInput = function () {
+const setupInput = function (connection) {
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
   
-    // Register "data" event listener for stdin
-    stdin.on("data", handleUserInput);
+    stdin.on("data", (key) => {
+      handleUserInput(key, connection);
+    });
   
     return stdin;
   };
   
-  // Handle user keyboard input
-  const handleUserInput = function (key) {
-    if (key === '\u0003') { // '\u0003' represents CTRL + C
+  const handleUserInput = function (key, connection) {
+    if (key === '\u0003') {
       process.exit();
     }
     // Handle other user input for controlling the snake
+    // For example, you can send move commands to the server based on user input
+    if (key === 'w') {
+      connection.write("Move: up");
+    } else if (key === 'a') {
+      connection.write("Move: left");
+    } else if (key === 's') {
+      connection.write("Move: down");
+    } else if (key === 'd') {
+      connection.write("Move: right");
+    }
   };
   
-  // Export the setupInput function
   module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,58 +1,20 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connectToServer = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // Handle incoming data
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
-
-  // Handle the connect event
-  conn.on("connect", () => {
-    console.log("Connected to server");
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connectToServer();
-
 const { connect } = require("./client");
-
 const { setupInput } = require("./input");
 
-
-const connection = connect(); // This line now uses the imported connect function
+console.log("Connecting ...");
+const connection = connect();
 
 // Setup user input handling
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  // Register "data" event listener for stdin
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+const input = setupInput(connection);
 
 // Handle user keyboard input
-const handleUserInput = function (key) {
-  if (key === '\u0003') { // '\u0003' represents CTRL + C
+input.handleUserInput = function (key) {
+  if (key === '\u0003') {
     process.exit();
   }
   // Handle other user input for controlling the snake
+  // For example, you can add more commands here
 };
 
 // Call setupInput to enable user input handling
-setupInput();
+input.handleUserInput = input.handleUserInput.bind(input); // Bind handleUserInput to the input object
