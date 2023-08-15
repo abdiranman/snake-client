f9552724d9a29067b7070a1eddae1a70b9c8e032
abdiranman <abdirahman.abdi330@.com>
2023-08-15 11:41:06 -0400

    add

--- play.js
+++ play.js
@@ -29,3 +29,27 @@ connectToServer();
 const { connect } = require("./client");
 
 const connection = connect(); // This line now uses the imported connect function
+
+// Setup user input handling
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // Register "data" event listener for stdin
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+// Handle user keyboard input
+const handleUserInput = function (key) {
+  if (key === '\u0003') { // '\u0003' represents CTRL + C
+    process.exit();
+  }
+  // Handle other user input for controlling the snake
+};
+
+// Call setupInput to enable user input handling
+setupInput();
