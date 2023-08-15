89cc98204f9ce1538056a8205a932f248aaab1f4
abdiranman <abdirahman.abdi330@.com>
2023-08-15 11:32:38 -0400

    add

--- client.js
+++ client.js
@@ -13,12 +13,28 @@ const connect = function () {
     console.log("Successfully connected to game server");
     // Send your name to the server
     conn.write("Name: AAA"); // Replace "XYZ" with your initials
-  });
 
+    // Send move commands to the server
+    // Move up
+    conn.write("Move: up");
+
+    // Move down after a delay
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 1000); // Delay of 1000ms (1 second)
+
+    // Move right after a longer delay
+    setTimeout(() => {
+      conn.write("Move: right");
+    }, 2000); // Delay of 2000ms (2 seconds)
+
+    // Move left after an even longer delay
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 3000); // Delay of 3000ms (3 seconds)
+  });
 
   return conn;
 };
 
-// Export the connect function using ES6 Shorthand syntax
 module.exports = { connect };
-
